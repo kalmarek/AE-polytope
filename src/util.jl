0df5bcfe250c1ace9d9118e2eec72256071167fd
kalmarek <kalmar@amu.edu.pl>
2019-12-04 19:33:45 +0100

    throw a meaningful error in number_of_vertices

--- src/util.jl
+++ src/util.jl
@@ -70,5 +70,6 @@ end
 
 function number_of_vertices(dim, hausdorff_dist; p=0.05, ε=0.1)
     n = (1 + 2/dim*log(2/p))*(ℯ/(ℯ-1))*1/(ε^2*(1-hausdorff_dist)^2)
+    n > typemax(Int) && throw("Number of vertices exceeds $($typemax(Int)). Try increasing Hausdorff distance, p, or ε!")
     return ceil(Int, n)
 end
